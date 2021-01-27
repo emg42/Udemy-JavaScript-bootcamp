387a86d9fa90f64b647bc0d33dac804f84554c3d
Erin Grady <erinmariegrady@gmail.com>
2021-01-27 13:10:09 -0600

    arrays challenge complete

--- arrays/todos.js
+++ arrays/todos.js
@@ -1,13 +1,36 @@
-let todos = [
-  "study code",
-  "go to work",
-  "do work",
-  "drink water",
-  "take technical interview",
-];
 // 1. Convert the array of strings to an array of objects -> text -> completed
 // 2. Create a function to remove a todo by text value
 
-todos.forEach(function (todo, index) {});
+const todos = [
+  {
+    text: "study code",
+    completed: true,
+  },
+  {
+    text: "go to work",
+    completed: false,
+  },
+  {
+    text: "do work",
+    completed: true,
+  },
+  {
+    text: "drink water",
+    completed: false,
+  },
+  {
+    text: "take technical interview",
+    completed: true,
+  },
+];
+
+const deleteTodo = function (todos, todoText) {
+  const index = todos.findIndex(function (todo) {
+    return todo.text.toLowerCase() === todoText.toLowerCase();
+  });
+  if (index > -1) {
+    todos.splice(index, 1);
+  }
+};
 deleteTodo(todos, "do work");
 console.log(todos);
